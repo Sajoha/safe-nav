3bc56609255c4a38be02422dcbd47125dcd5c75b
Sam Haig <samjhaig@gmail.com>
2017-03-15 15:05:21 +0000

    Introduction Of Appium UI Tests
    Added a baseline Appium infrastructure to enable UI testing potential

---
+++ Appium/helpers/setup.js
@@ -0,0 +1,37 @@
+'use strict';
+
+class Setup {
+	constructor() {
+		this.wd = require('wd');
+		this.chai = require('chai');
+		this.chaiAsPromised = require('chai-as-promised');
+		this.colors = require('colors');
+
+		// enabling chai assertion style: https://www.npmjs.com/package/chai-as-promised#node
+		this.chai.use(this.chaiAsPromised);
+		this.chai.should();
+		// enables chai assertion chaining
+		this.chaiAsPromised.transferPromiseness = this.wd.transferPromiseness;
+	}
+
+	// returns web driver module
+	getWd() {
+		return this.wd;
+	}
+
+	// prints out driver logging
+	logging(driver) {
+		// See whats going on
+		driver.on('status', info => {
+			console.log(this.colors.cyan(info));
+		});
+		driver.on('command', (meth, path, data) => {
+			console.log(` > ${this.colors.yellow(meth)} ${this.colors.grey(path)} ${data || ''}`);
+		});
+		driver.on('http', (meth, path, data) => {
+			console.log(` > ${this.colors.magenta(meth)} ${path} ${this.colors.grey(data || '')}`);
+		});
+	}
+}
+
+module.exports = Setup;
---
+++ Appium/test/ios_test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const Setup = require('../helpers/setup.js');
+
+let
+	driver = null,
+	webdriver = null;
+
+before('suite setup', function () {
+	// the webdriver takes a while to setup; mocha timeout is set to 5 minutes
+	this.timeout(300000);
+
+	const setup = new Setup();
+
+	webdriver = setup.getWd();
+
+	// appium local server
+	driver = webdriver.promiseChainRemote({
+		host: 'localhost',
+		port: 4723
+	});
+
+	// turn on logging for the driver
+	setup.logging(driver);
+
+	// specify target test app and ios simulator
+	return driver.init({
+		automationName: 'XCUITest',
+		platformName: 'iOS',
+		deviceName: 'iPhone 7',
+		platformVersion: '10.2',
+		app: '/Users/shaig/Workspace/GitHub/safe-nav/build/iphone/build/Products/Debug-iphonesimulator/safeNav.app',
+		noReset: true // doesn't kill the simulator
+	});
+});
+
+after('suite teardown', function () {
+	return driver.quit();
+});
+
+describe('OAuthApplication', function() {
+	this.timeout(300000);
+
+	it('Should type in the text box', function() {
+		return driver
+			.elementById('totxt')
+			.click()
+			.keys('Sam!');
+	});
+
+	it('Should launch the Map', function() {
+		return driver
+			//.elementById('goBtn')
+			.elementByName('Plot Route!')
+			.click()
+			.sleep(3600);
+	});
+});
