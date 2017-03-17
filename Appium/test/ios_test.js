de33b18a326df1cb73a79936ab75a540435cd6c9
Sam Haig <samjhaig@gmail.com>
2017-03-17 13:36:25 +0000

    MapBox Updates, Appium Work & Whitespace
    Integrated an adapter to start using MapBox on the App instead of
    native maps in the hope of there being more functionality through the
    API.

--- Appium/test/ios_test.js
+++ Appium/test/ios_test.js
@@ -3,56 +3,56 @@
 const Setup = require('../helpers/setup.js');
 
 let
-	driver = null,
-	webdriver = null;
+    driver = null,
+    webdriver = null;
 
-before('suite setup', function () {
-	// the webdriver takes a while to setup; mocha timeout is set to 5 minutes
-	this.timeout(300000);
+before('suite setup', function() {
+    // the webdriver takes a while to setup; mocha timeout is set to 5 minutes
+    this.timeout(300000);
 
-	const setup = new Setup();
+    const setup = new Setup();
 
-	webdriver = setup.getWd();
+    webdriver = setup.getWd();
 
-	// appium local server
-	driver = webdriver.promiseChainRemote({
-		host: 'localhost',
-		port: 4723
-	});
+    // appium local server
+    driver = webdriver.promiseChainRemote({
+        host: 'localhost',
+        port: 4723
+    });
 
-	// turn on logging for the driver
-	setup.logging(driver);
+    // turn on logging for the driver
+    setup.logging(driver);
 
-	// specify target test app and ios simulator
-	return driver.init({
-		automationName: 'XCUITest',
-		platformName: 'iOS',
-		deviceName: 'iPhone 7',
-		platformVersion: '10.2',
-		app: '/Users/shaig/Workspace/GitHub/safe-nav/build/iphone/build/Products/Debug-iphonesimulator/safeNav.app',
-		noReset: true // doesn't kill the simulator
-	});
+    // specify target test app and ios simulator
+    return driver.init({
+        automationName: 'XCUITest',
+        platformName: 'iOS',
+        deviceName: 'iPhone SE',
+        platformVersion: '10.2',
+        app: '/Users/sam/Workspace/GitHub/safe-nav/build/iphone/build/Products/Debug-iphonesimulator/safeNav.app',
+        noReset: true // doesn't kill the simulator
+    });
 });
 
-after('suite teardown', function () {
-	return driver.quit();
+after('suite teardown', function() {
+    return driver.quit();
 });
 
 describe('OAuthApplication', function() {
-	this.timeout(300000);
+    this.timeout(300000);
 
-	it('Should type in the text box', function() {
-		return driver
-			.elementById('totxt')
-			.click()
-			.keys('Sam!');
-	});
+    it('Should type in the text box', function() {
+        return driver
+            .elementById('com.shaig.safenav/id:totxt')
+            .click()
+            .keys('Sam!');
+    });
 
-	it('Should launch the Map', function() {
-		return driver
-			//.elementById('goBtn')
-			.elementByName('Plot Route!')
-			.click()
-			.sleep(3600);
-	});
+    it('Should launch the Map', function() {
+        return driver
+            //.elementById('goBtn')
+            .elementByName('Plot Route!')
+            .click()
+            .sleep(3600);
+    });
 });
