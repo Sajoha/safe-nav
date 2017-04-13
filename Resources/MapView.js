fc65ae8b98459f0d02b3d2486669db74462050a3
SamJHaig <samjhaig@gmail.com>
2017-04-13 21:25:39 +0100

    Plots the route using placed pins as start and end points

--- Resources/MapView.js
+++ Resources/MapView.js
@@ -2,48 +2,97 @@
  * @author Sam Haig
  */
 
+// TODO: Make pins placeable
+
+// TODO: Remove old route when a new one is created
+
+// TODO: Find out how long press works
+
+// TODO: Import this back into the app.js
+
 var Map = require('ti.map');
 
 function MapView() {
     // Create the route window object
     var mapWin = Ti.UI.createWindow();
-    var mapview = Map.createView();
+    var map = Map.createView();
 
-    // Get the users current location
+    // Get the users current location, and configure the map view
     Titanium.Geolocation.getCurrentPosition(function(e) {
-        // Create a map object, and insert it in the window
-        mapview.mapType = Map.NORMAL_TYPE,
-            mapview.region = {
+        map.mapType = Map.NORMAL_TYPE,
+            map.region = {
                 latitude: e.coords.latitude,
                 longitude: e.coords.longitude,
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01
             },
-            mapview.animate = true,
-            mapview.regionFit = true,
-            mapview.userLocation = true,
-            mapview.annotations = []
+            map.animate = true,
+            map.regionFit = true,
+            map.userLocation = true
     });
-    mapWin.add(mapview);
+    mapWin.add(map);
 
+    // Button for setting the view back to the current location
+    var currentButton = Ti.UI.createButton({
+        title: 'Calculate',
+        top: '80%',
+        left: '5%',
+        width: 100,
+        height: 50
+    });
     // Collect the route points, and draw them onto the map
-    getRoute(function(routePoints) {
-        var route = Map.createRoute({
-            points: routePoints,
-            color: "#f00",
-            width: 5.0
-        });
-        mapview.addRoute(route);
-    });
+    currentButton.addEventListener('click', function(e) {
+        // Method variables
+        var startNode,
+            endNode;
 
+        // Extract the start node and the end node
+        for(var i = 0; i < map.annotations.length; i++) {
+            if(map.annotations[i].title === 'Start') {
+                startNode = map.annotations[i];
+            } else if(map.annotations[i].title === 'End') {
+                endNode = map.annotations[i];
+            }
+        }
+
+        // Pass the pins to the method extraction function, then draw the route
+        getRoute(startNode, endNode, function(routePoints) {
+            map.addRoute(Map.createRoute({
+                points: routePoints,
+                color: "#f00",
+                width: 5.0
+            }));
+        });
+    });
+    map.add(currentButton);
+
+    // Open the window
     mapWin.open();
+
+    // Add an start pin, currently in a street away from the centre of Huddersfield
+    map.addAnnotation(Map.createAnnotation({
+        latitude: 53.643306,
+        longitude: -1.758004,
+        title: 'Start',
+        animate: true,
+        pincolor: Map.ANNOTATION_RED
+    }));
+
+    // Add an end pin, currently on the University of Huddersfield
+    map.addAnnotation(Map.createAnnotation({
+        latitude: 53.640894,
+        longitude: -1.778847,
+        title: 'End',
+        animate: true,
+        pincolor: Map.ANNOTATION_GREEN
+    }));
 };
 module.exports = MapView;
 
-function getRoute(done) {
+function getRoute(startPin, endPin, done) {
     // Query the API server with the given coordinates
-    var url = 'http://192.168.0.39:5000/route/v1/driving/13.388860,52.517037;13.385983,52.496891?steps=true'
-    // var url = "http://192.168.0.39:5000/route/v1/driving/53.848611,-1.663822;53.870108,-1.727657?steps=true";
+    var url = 'http://192.168.0.39:5000/route/v1/driving/' + startPin.longitude + ',' + startPin.latitude + ';' + endPin.longitude + ',' + endPin.latitude + '?steps=true';
+    Ti.API.info('Route request sent to: ' + url);
 
     // Instantiate the routePoints object
     var routePoints = [];
--- Resources/app.js
+++ Resources/app.js
@@ -1,12 +1,3 @@
 var MapView = require('MapView');
 
 var ui = new MapView();
-
-// ui.longPressOnMap
-
-// ui.setAnnotation({
-//     latitude: 18.467354,
-//     longitude: -91.903534,
-//     title: 'Test Title',
-//     subtitle: 'Subtitle'
-// });
