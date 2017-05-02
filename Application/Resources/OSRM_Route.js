aa8f92d5ea340c8576aecda3cf9e178556396ece
Sam Haig <samjhaig@gmail.com>
2017-05-02 13:45:27 +0100

    Remove wait period from Police API call

--- Application/Resources/OSRM_Route.js
+++ Application/Resources/OSRM_Route.js
@@ -11,43 +11,42 @@
  * @param {function} done - Callback for when the job is complete
  ******************************************************************************/
 function OSRM_Route(startPin, endPin, serverAddress, done) {
-    // Query the API server with the given coordinates
-    var url = 'http://' + serverAddress + ':5000/route/v1/walking/' + startPin.longitude + ',' + startPin.latitude + ';' + endPin.longitude + ',' + endPin.latitude + '?steps=true&alternatives=true';
+	// Query the API server with the given coordinates
+	var url = 'http://' + serverAddress + ':5000/route/v1/walking/' + startPin.longitude + ',' + startPin.latitude + ';' + endPin.longitude + ',' + endPin.latitude + '?steps=true&alternatives=true';
 
-    // Instantiate the routePoints object
-    var routePoints = [];
+	// Instantiate the routePoints object
+	var routePoints = [];
 
-    // Parse the response
-    var client = Ti.Network.createHTTPClient({
-        onload: function(e) {
-            var response = JSON.parse(this.responseText);
-            // Concatinate all of the JSON coordinates into an array
-            for(var i = 0; i < response.routes[0].legs[0].steps.length; i++) {
-                for(var k = 0; k < response.routes[0].legs[0].steps[i].intersections.length; k++) {
-                    var coord = {
-                        latitude: response.routes[0].legs[0].steps[i].intersections[k].location[1],
-                        longitude: response.routes[0].legs[0].steps[i].intersections[k].location[0],
-                        incidents: 0
-                    };
-                    routePoints.push(coord);
-                }
-            }
-            // Return the results
-            done(routePoints);
-        },
-        onerror: function(e) {
-            Ti.API.info(e.error);
-            done(null)
-        },
-        // Timeout of 10 seconds
-        timeout: 10000
-    });
+	// Parse the response
+	var client = Ti.Network.createHTTPClient({
+		onload: function(e) {
+			var response = JSON.parse(this.responseText);
+			// Concatinate all of the JSON coordinates into an array
+			for(var i = 0; i < response.routes[0].legs[0].steps.length; i++) {
+				for(var k = 0; k < response.routes[0].legs[0].steps[i].intersections.length; k++) {
+					var coord = {
+						latitude: response.routes[0].legs[0].steps[i].intersections[k].location[1],
+						longitude: response.routes[0].legs[0].steps[i].intersections[k].location[0],
+						incidents: 0
+					};
+					routePoints.push(coord);
+				}
+			}
+			// Return the results
+			done(routePoints);
+		},
+		onerror: function(e) {
+			done(null)
+		},
+		// Timeout of 10 seconds
+		timeout: 10000
+	});
 
-    // Prepare the connection.
-    client.open("GET", url);
+	// Prepare the connection.
+	client.open("GET", url);
 
-    // Send the request.
-    client.send();
+	// Send the request.
+	client.send();
 }
 
 module.exports = OSRM_Route;
--- Application/Resources/Police_Check.js
+++ Application/Resources/Police_Check.js
@@ -8,53 +8,52 @@
  * @param {function} done - Callback for when the job is complete
  ******************************************************************************/
 function Police_Check(lat, long, done) {
-    // Get the current date
-    var count = 0,
-        today = new Date();
+	// Get the current date
+	var count = 0,
+		today = new Date();
 
-    // Go back 4 months to the latest Police record
-    today.setMonth(today.getMonth() - 4);
+	// Go back 4 months to the latest Police record
+	today.setMonth(today.getMonth() - 4);
 
-    // Create a string of YYYY-DD format for the API URL.
-    var month = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2);
+	// Create a string of YYYY-DD format for the API URL.
+	var month = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2);
 
-    // Query the police database for events
-    var url = 'https://data.police.uk/api/crimes-street/all-crime?lat=' + lat + '&lng=' + long + '&date=' + month;
-    Ti.API.debug('Police: ' + url);
+	// Query the police database for events
+	var url = 'https://data.police.uk/api/crimes-street/all-crime?lat=' + lat + '&lng=' + long + '&date=' + month;
+	Ti.API.debug('Police: ' + url);
 
-    // Query the API server, and count the number of incidents
-    var client = Ti.Network.createHTTPClient({
-        onload: function(e) {
-            var total = 0;
-            total += (this.responseText.match(/anti-social-behaviour/g) || []).length;
-            total += (this.responseText.match(/bicycle-theft/g) || []).length;
-            total += (this.responseText.match(/burglary/g) || []).length;
-            total += (this.responseText.match(/criminal-damage-arson/g) || []).length;
-            total += (this.responseText.match(/drugs/g) || []).length;
-            total += (this.responseText.match(/other-theft/g) || []).length;
-            total += (this.responseText.match(/possession-of-weapons/g) || []).length;
-            total += (this.responseText.match(/public-order/g) || []).length;
-            total += (this.responseText.match(/robbery/g) || []).length;
-            total += (this.responseText.match(/shoplifting/g) || []).length;
-            total += (this.responseText.match(/theft-from-the-person/g) || []).length;
-            total += (this.responseText.match(/vehicle-crime/g) || []).length;
-            total += (this.responseText.match(/violent-crime/g) || []).length;
-            total += (this.responseText.match(/other-crime/g) || []).length;
-            done(total);
-        },
-        onerror: function(e) {
-            Ti.API.info(e.error);
-            done(null);
-        },
-        // Timeout of 10 seconds
-        timeout: 10000
-    });
+	// Query the API server, and count the number of incidents
+	var client = Ti.Network.createHTTPClient({
+		onload: function(e) {
+			var total = 0;
+			total += (this.responseText.match(/anti-social-behaviour/g) || []).length;
+			total += (this.responseText.match(/bicycle-theft/g) || []).length;
+			total += (this.responseText.match(/burglary/g) || []).length;
+			total += (this.responseText.match(/criminal-damage-arson/g) || []).length;
+			total += (this.responseText.match(/drugs/g) || []).length;
+			total += (this.responseText.match(/other-theft/g) || []).length;
+			total += (this.responseText.match(/possession-of-weapons/g) || []).length;
+			total += (this.responseText.match(/public-order/g) || []).length;
+			total += (this.responseText.match(/robbery/g) || []).length;
+			total += (this.responseText.match(/shoplifting/g) || []).length;
+			total += (this.responseText.match(/theft-from-the-person/g) || []).length;
+			total += (this.responseText.match(/vehicle-crime/g) || []).length;
+			total += (this.responseText.match(/violent-crime/g) || []).length;
+			total += (this.responseText.match(/other-crime/g) || []).length;
+			done(total);
+		},
+		onerror: function(e) {
+			done(null);
+		},
+		// Timeout of 10 seconds
+		timeout: 10000
+	});
 
-    // Prepare the connection
-    client.open("GET", url);
+	// Prepare the connection
+	client.open("GET", url);
 
-    // Send the request
-    client.send();
+	// Send the request
+	client.send();
 }
 
 module.exports = Police_Check;
